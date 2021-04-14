2652633c15dd2324220f6e4745c04824e45b59bd
sjgg555 <sjgg555@gmail.com>
2021-04-14 11:46:36 +0100

    split generating signed distance field out

--- gui_images/main.py
+++ gui_images/main.py
@@ -10,6 +10,7 @@ from PIL import Image
 import os
 import math
 import numpy as np
+import generate_sdf as sdf
 
 
 def display_input_image(sender, data):
@@ -25,58 +26,9 @@ def display_input_image(sender, data):
         
     display_output_sdf(file_path)
     
-
-def calculate_colour(distance):
-    white = 255
-    colour = white / distance 
-    cutoff = 5
-    return np.clip(colour, 0, cutoff)
-
-
-def generate_sdf(image):
-    """Per pixel, find the distance to the closest pixel with a specifed colour.
-    Save the value to the pixel as greyscale"""
-    target = np.array([255,255,255])
-    output = image.copy()
-    for x in range(0, image.height):
-        for y in range(0, image.width):
-            distance = get_distance_to_colour(x, y, image, target)
-            output.setpixel(calculate_colour(distance))
-    return output
-
-
-def get_spiral(X_size, Y_size, x, y):
-    dx = 0
-    dy = -1
-    x_size = 2 * X_size
-    y_size = 2 * Y_size
-    for _ in range(max(X_size, Y_size)**2):
-        if (-x_size/2 < x <= x_size/2) and (-y_size/2 < y <= y_size/2):
-            yield x, y
-        if x == y or (x < 0 and x == -y) or (x > 0 and x == 1-y):
-            dx, dy = -dy, dx
-        x, y = x+dx, y+dy
-    return x, y
-
-
-def get_distance_to_colour(x, y, image, target):
-    """spirals out from current pixel, return when the first pixel of colour is found"""       
-    distance = -1
-    spiral = get_spiral(image.width, image.height, x, y)
-    for _ in range(image.width * image.height):
-        x_t, y_t = next(spiral)
-        x_prime = x + x_t 
-        y_prime = y + y_t
-        if (-1 < x_prime < image.width) and (-1 < y_prime < image.height):
-            spiral_color = np.array(image.getpixel((x_prime, y_prime)))
-        if np.equal(spiral_color, target):
-            distance = math.floor(math.sqrt(x_t**2, y_t**2))
-    return distance
-
-
 def display_output_sdf(file_path):
     """Takes an input image and outputs an sdf image,"""
-    img = generate_sdf(Image.open(file_path).convert("LA"))
+    img = sdf.generate_sdf(Image.open(file_path).convert("LA"))
     
     dpg_image = []
     for i in range(0, img.height):
